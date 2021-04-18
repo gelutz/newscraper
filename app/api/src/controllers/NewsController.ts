9c99ee04084add8c70dd4ff4d5fb771c1e892a76
gelutz <gabriel.lutz5@gmail.com>
2021-04-18 17:11:44 -0300

    Create e Read do CRUD funcionando; esperando melhorar os testes

--- app/api/src/controllers/NewsController.ts
+++ app/api/src/controllers/NewsController.ts
@@ -1,32 +1,42 @@
-import { Request, Response } from "express"
-import { getRepository } from "typeorm"
+import { Request, Response } from "express";
+import { getRepository } from "typeorm";
 
-import New from "../models/News"
+import New from "../models/News";
 
-class NewsController {
-	async index(req: Request, res: Response) {
-		const repository = getRepository(New)
-
-		const { id, title, link } = req.body
-
-		const returnedNews = await repository.findOne({ id, title, link })
-
-		res.send({ ...returnedNews })
-	}
-
-	async store(req: Request, res: Response) {
-		const repository = getRepository(New)
-
-		const data = req.body
-		try {
-			const updated = await repository.save(data)
-			console.log(updated)
-
-			res.send({ ...updated })
-		} catch (error) {
-			console.error(error)
-		}
-	}
+// 04/2021 - express não vem mais com body-parser, req.body é undefined
+interface NewsRequest extends Request {
+    body: {
+        id: number;
+        title: string;
+        link: string;
+        from: string;
+        date: Date;
+    };
 }
 
-export default new NewsController()
+class NewsController {
+    async index(req: NewsRequest, res: Response) {
+        const repository = getRepository(New);
+
+        const { id, title, link } = req.body;
+
+        const returnedNews = await repository.findOne({ id, title, link });
+
+        res.send({ ...returnedNews });
+    }
+
+    async store(req: NewsRequest, res: Response) {
+        const repository = getRepository(New);
+
+        const data = req.body;
+        try {
+            const updated = await repository.save(data);
+
+            res.send({ ...updated });
+        } catch (error) {
+            console.error(error);
+        }
+    }
+}
+
+export default new NewsController();
