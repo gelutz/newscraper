c57a7ab8388c604499d6598d41021f545bf666d3
gelutz <gabriel.lutz5@gmail.com>
2021-08-25 07:58:34 -0300

    feat:  created opaqueToken and tokenPair functions

--- app/server/src/utils/token.ts
+++ app/server/src/utils/token.ts
@@ -1,9 +1,28 @@
 import { sign } from "jsonwebtoken";
-
-export async function generateToken({ ...payload }): Promise<string> {
+import { randomBytes } from "crypto";
+import Allowlist from "../repositories/Allowlist";
+import endOfWeek from "date-fns/endOfWeek";
+export async function getAccessToken({ ...payload }): Promise<string> {
     const token = sign({ ...payload }, process.env.JWT_KEY!, {
         expiresIn: "5m",
     });
 
     return token;
+}
+
+export function getOpaqueToken(): string {
+    return randomBytes(16).toString("hex");
+}
+
+export async function getTokenPair({
+    ...accessPayload
+}): Promise<[string, string]> {
+    const accessToken = await getAccessToken(accessPayload);
+    const opaqueToken = getOpaqueToken();
+
+    await Allowlist.save(opaqueToken, {
+        exp: endOfWeek(new Date()).toLocaleString(),
+    });
+
+    return [accessToken, opaqueToken];
 }
