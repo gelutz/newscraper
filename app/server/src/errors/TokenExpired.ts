2444235614629af99e700fbd06fa58b0655c7758
gelutz <gabriel.lutz5@gmail.com>
2021-08-25 07:57:13 -0300

    feat: created TokenExpired error

---
+++ app/server/src/errors/TokenExpired.ts
@@ -0,0 +1,14 @@
+import CustomError from "./CustomError";
+
+export default class TokenExpiredError extends CustomError {
+    readonly status: number;
+    readonly message: string;
+    readonly name: string;
+
+    constructor(type: "access" | "opaque") {
+        super();
+        this.message = `O token ${type} está expirado.`;
+        this.name = "TokenExpired";
+        this.status = 401;
+    }
+}
