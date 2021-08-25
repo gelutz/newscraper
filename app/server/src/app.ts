3cdee846ac51dd06e00d64b4521c556fdbcf4d6a
gelutz <gabriel.lutz5@gmail.com>
2021-08-25 07:51:02 -0300

    feat: added x-powered middleware

--- app/server/src/app.ts
+++ app/server/src/app.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import cors from "cors";
 
+import { xPowered } from "./middlewares/xPowered";
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(xPowered);
 
 export { app };
--- app/server/src/middlewares/xPowered.ts
+++ app/server/src/middlewares/xPowered.ts
@@ -1,9 +1,8 @@
 import { Request, Response, NextFunction } from "express";
 
 export const xPowered = (_: Request, res: Response, next: NextFunction) => {
+    next();
     res.set({
         "X-Powered-By": "The Lutz",
     });
-
-    return next();
 };
