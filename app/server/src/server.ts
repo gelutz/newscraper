40b9bad5e46b8279a8458e504083961a3cd45a09
gelutz <gabriel.lutz5@gmail.com>
2021-08-25 07:52:21 -0300

    feat: start postgres server asynchronously

--- app/server/src/server.ts
+++ app/server/src/server.ts
@@ -1,24 +1,17 @@
 import "express-async-errors";
-
 import * as postgresClient from "./database/connections/postgres";
 import * as redisClient from "./database/connections/redis";
 
 import Routes from "./routes";
 
-import { errorHandler } from "./middlewares/Error";
-import { xPowered } from "./middlewares/xPowered";
-
 import { app } from "./app";
-
-app.use(xPowered);
+import { errorHandler } from "./middlewares/Error";
 
 Routes(app);
-
-app.use(errorHandler);
+errorHandler(app);
 
 redisClient.start();
+postgresClient.start(); // async
 
-postgresClient.start().then(() => {
-    const port = process.env.SERVER_PORT;
-    app.listen(port, () => console.log(`Server running at port ${port}`));
-});
+const port = process.env.SERVER_PORT;
+app.listen(port, () => console.log(`Server running at port ${port}`));
