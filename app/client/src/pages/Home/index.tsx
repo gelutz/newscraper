cd60010adb2aad286d853aa943644a2509d9ef49
gelutz <gabriel.lutz5@gmail.com>
2021-08-29 13:45:01 -0300

    client@feat: added logout button on home page

--- app/client/src/pages/Home/index.tsx
+++ app/client/src/pages/Home/index.tsx
@@ -6,15 +6,16 @@ import News from "../../@types/News";
 import connection from "../../api/connection";
 import NewsList from "../../components/NewsList";
 import Header from "../../components/Header";
+import { useAuth } from "../../hooks/auth";
 
 // import { Container } from "./styles";
 
 const Home: React.FC = () => {
   const { register, handleSubmit, getValues } = useForm<{ search: string }>();
-
+  const { signOut } = useAuth();
   const [news, setNews] = useState<News[]>([]);
 
-  const onSearch = handleSubmit(async (data) => {
+  const onSearch = handleSubmit(async () => {
     const values = getValues();
 
     const returned = await connection.get<News[]>("/news/search", {
@@ -48,19 +49,19 @@ const Home: React.FC = () => {
   }, []);
 
   return (
-    <div className="p-8 bg-gray-900 shadow h-full">
+    <div>
       <Header>
         <form onSubmit={onSearch}>
           <input
             {...register("search", { required: false })}
             type="text"
-            className="w-full py-3 pl-10 pr-4 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
             placeholder="Search"
             id="search"
           />
         </form>
+        <button onClick={signOut}>Logout</button>
       </Header>
-      <div className="list w-full mt-8 bg-gray-600">
+      <div>
         <NewsList news={news} />
       </div>
     </div>
