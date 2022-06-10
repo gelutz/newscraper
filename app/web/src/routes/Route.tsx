a0d6d31ed260de451fb127fd91bb228a7e8ca7e8
gelutz <gabriel.lutz5@gmail.com>
2022-06-10 02:43:28 -0300

    fuckfuckfuckfuck

---
+++ app/web/src/routes/Route.tsx
@@ -0,0 +1,30 @@
+import {
+    Route as ReactDOMRoute,
+    RouteProps as ReactDOMRouteProps
+} from "react-router-dom";
+
+// import { Routes } from 'react-router'
+
+import { useAuth } from "../hooks/auth";
+
+interface RouteProps extends ReactDOMRouteProps {
+    isPrivate?: boolean;
+    component: React.ComponentType;
+}
+
+const Route: React.FC<RouteProps> = ({
+    isPrivate = false,
+    component: Component,
+    ...rest
+}) => {
+    const { user } = useAuth();
+
+    return (
+        <ReactDOMRoute
+            {...rest}
+            element={<Component />}
+        />
+    );
+};
+
+export default Route;
---
+++ app/web/src/routes/index.tsx
@@ -0,0 +1,16 @@
+import { Routes as Switch } from 'react-router'
+import Route from "./Route";
+
+import Home from "../pages/Home";
+import Login from "../pages/Login/";
+
+function Routes() {
+    return (
+        <Switch>
+            <Route path="/" exact component={Home} isPrivate />
+            <Route path="/login" component={Login} />
+        </Switch>
+    );
+}
+
+export default Routes;
